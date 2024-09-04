c75356ab4a3367da597d6dba8fc65c971d4c6e82
FVolaco <fvolaco@gmail.com>
2024-09-04 12:24:42 -0300

    primeiro projeto angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent}, 
+  {path: 'lista', component: ProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
